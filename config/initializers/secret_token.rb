32060bf108ad8a86a131dc66ec412559126279d4
heigel <heigel@circinus.uberspace.de>
2014-02-11 15:38:15 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tillmannheigel::Application.config.secret_key_base = 'bdbf173e4fce696840a9ee763af0f791d2cdeb040e01071e4fbdfbf09451f7a8cb488a6f34f04efaab9b830a1d2074c2298e07c4eafa10dee17c34ed73064649'
