9515a0f1ecb9c83b0c4f621d59a75f0adaf53424
Tillmann <heigel@gmino.de>
2014-02-11 19:55:50 +0100

    reveiled last things

--- config/initializers/add_to_asset_paths.rb
+++
@@ -1,3 +0,0 @@
-# Ralph: http://guides.rubyonrails.org/asset_pipeline.html is wrong
-# Tillmann: https://www.ruby-forum.com/topic/3300305
-Rails.application.config.assets.paths << (Rails.root.join("fcgi-bin","tillmannheigel", "assets")).to_s
