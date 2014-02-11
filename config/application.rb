3c6585dcd199bc1f4a7656b9215d24dc06a1b41b
Tillmann <heigel@gmino.de>
2014-02-11 19:00:04 +0100

    application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Tillmannheigel
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
-    config.assets.prefix = '/../assets'
+    config.assets.prefix = '/tillmannheigel/assets'
   end
 end
