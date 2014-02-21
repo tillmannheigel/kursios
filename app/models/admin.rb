f5bef2a569cb2618884c54b95587e4ba42c20526
Tillmann <heigel@gmino.de>
2014-02-21 19:06:33 +0100

    let

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable, :validatable
+  devise :database_authenticatable, :trackable, :validatable, :registerable
 end
